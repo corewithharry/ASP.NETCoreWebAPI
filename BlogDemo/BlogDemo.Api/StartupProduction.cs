3b2c2dbe28ad5f982cad1e3d7e81154e6b54ad7a
RedStand <13810063423@126.com>
2019-12-12 10:30:37 +0800

    生产环境下配置HSTS服务

--- BlogDemo/BlogDemo.Api/StartupProduction.cs
+++ BlogDemo/BlogDemo.Api/StartupProduction.cs
@@ -16,11 +16,27 @@ namespace BlogDemo.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 5001;
+
+            });
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.ExcludedHosts.Add("example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseHsts();
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
