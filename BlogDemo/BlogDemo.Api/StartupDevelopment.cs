0999ac2f38fe2906fe0fc42e5d66367e3134119b
RedStand <13810063423@126.com>
2019-12-13 14:37:36 +0800

    配置项appsettings.json

--- BlogDemo/BlogDemo.Api/StartupDevelopment.cs
+++ BlogDemo/BlogDemo.Api/StartupDevelopment.cs
@@ -9,12 +9,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlogDemo.Api
 {
     public class StartupDevelopment
     {
+        public static IConfiguration Configuration { get; set; }
+
+        public StartupDevelopment(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,7 +31,8 @@ namespace BlogDemo.Api
 
             services.AddDbContext<MyContext>(options =>
             {
-                options.UseSqlite("Data Source=BlogDemo.db");
+                var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+                options.UseSqlite(connectionString);
             });
 
             services.AddHttpsRedirection(options=>
