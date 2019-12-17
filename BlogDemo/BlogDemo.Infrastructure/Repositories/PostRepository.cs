83e731741b429ac47474a60f5cf9964e47b7b024
RedStand <13810063423@126.com>
2019-12-17 15:27:51 +0800

    实现过滤查询

--- BlogDemo/BlogDemo.Infrastructure/Repositories/PostRepository.cs
+++ BlogDemo/BlogDemo.Infrastructure/Repositories/PostRepository.cs
@@ -20,7 +20,16 @@ namespace BlogDemo.Infrastructure.Repositories
 
         public async Task<PaginatedList<Post>> GetAllPostsAsync(PostParameters postParameters)
         {
-            var query = _myContext.Posts.OrderBy(x => x.Id);
+            //var query = _myContext.Posts.OrderBy(x => x.Id);
+            var query = _myContext.Posts.AsQueryable();
+            if (!string.IsNullOrEmpty(postParameters.Title))
+            {
+                var title = postParameters.Title.ToLowerInvariant();
+                query = query.Where(x => x.Title.ToLowerInvariant()==title);
+            }
+            query = query.OrderBy(x => x.Id);
+
+
             var count = await query.CountAsync();
             var data = await query
                 .Skip(postParameters.PageIndex * postParameters.PageSize)
